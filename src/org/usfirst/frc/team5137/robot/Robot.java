9bdb2870c06bcb341ea499f1fe061d1e50d7daf5
sclincoln27 <sclincoln27@192.168.0.9>
2018-01-20 21:02:10 -0800

    Cleaned up Robot hi

--- src/org/usfirst/frc/team5137/robot/Robot.java
+++ src/org/usfirst/frc/team5137/robot/Robot.java
@@ -18,47 +18,20 @@ public class Robot extends TimedRobot {
     Joystick jackBlack;
     public static DriveTrain driveTrain;
     public static OI oi;
-
-    //int driveMode; // 0 is arcade, 1 is tank
     
     public void robotInit() {
-    	RobotMap.init();
-    	driveTrain = new DriveTrain();
-    	/*80's music is so catchy
-    	 * that all other music is trashy
-    	 * hall and oats should have my childern
-    	 * throw them into the urn
-    	 */
-    	
-    	oi = new OI();
-        //driveMode = 0; //initiates to arcade drive mode
+	    	RobotMap.init();
+	    	driveTrain = new DriveTrain();
+	    	/*80's music is so catchy
+	    	 * that all other music is trashy
+	    	 * hall and oats should have my childern
+	    	 * throw them into the urn
+	    	 */	    	
+	    	oi = new OI();
     }
 
 	public void teleopPeriodic() {
 	    Scheduler.getInstance().run();
-	    
-	    /*if (rightBumperIsPressed) driveMode = 0;
-		if (leftBumperIsPressed) driveMode = 1;
-		
-		
-		if (yIsPressed) { 
-			shooterMotor.set(1);
-			Timer.delay(0.5);
-			feederMotor.set(-1);
-		}
-		if (bIsPressed) {
-			climber.set(1.0);
-		}
-		if (xIsPressed) {
-			intakeMotor.set(1);
-		}
-		else {
-			climber.set(0);
-			feederMotor.set(0);
-			shooterMotor.set(0);
-			intakeMotor.set(0);	
-		}*/
-	 	
 	}
 }
 
